5601b806dde0dc387dba437cf75eabbad39a15ff
OceanOCD <lienquan.3567@gmail.com>
2024-11-27 17:11:00 +0700

    Add files via upload

---
+++ student.js
@@ -0,0 +1,27 @@
+function splitList() {
+  const studentList = document
+    .getElementById("studentList")
+    .value.trim()
+    .split("\n")
+    .filter(Boolean);
+  const shuffledList = studentList.sort(() => 0.5 - Math.random());
+  const class1 = [];
+  const class2 = [];
+
+  shuffledList.forEach((student, index) => {
+    if (index % 2 === 0) {
+      class1.push(student);
+    } else {
+      class2.push(student);
+    }
+  });
+
+  document.getElementById("class1").textContent = class1
+    .sort()
+    .map((student, i) => `${i + 1}. ${student}`)
+    .join("\n");
+  document.getElementById("class2").textContent = class2
+    .sort()
+    .map((student, i) => `${i + 1}. ${student}`)
+    .join("\n");
+}
